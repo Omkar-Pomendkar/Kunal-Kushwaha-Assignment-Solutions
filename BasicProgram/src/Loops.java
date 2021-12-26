cdd57b937ec715ee1698cc997570702c1e37cb18
Omkar-Pomendkar <omkarpomendkar100@gmail.com>
2021-12-26 22:10:11 +0530

    Loops

---
+++ BasicProgram/src/Loops.java
@@ -0,0 +1,21 @@
+public class Loops {
+    public static void main(String[] args) {
+//        Q: Print 1 to 5
+//        for(int i=1;i<=5;i++)
+//        {
+//            System.out.println(i);
+//    }
+//        int i = 1;
+//        while(i<=5)
+//        {
+//            System.out.println(i);
+//            i++;
+//        }
+        int i = 1;
+  do{
+
+      System.out.println(i);
+      i++;
+  }while(i<5);
+    }
+}
