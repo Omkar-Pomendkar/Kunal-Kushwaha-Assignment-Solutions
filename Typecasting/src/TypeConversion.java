664845d691ffe54b33ffdbf6d9473408609c7a9f
Omkar-Pomendkar <omkarpomendkar100@gmail.com>
2021-12-26 18:43:02 +0530

    Typecasting

---
+++ Typecasting/src/TypeConversion.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class TypeConversion {
+    public static void main(String[] args) {
+        System.out.println("TypeCasting");
+        Scanner sc = new Scanner(System.in);
+        float num = sc.nextInt();
+        System.out.println((float)num);
+
+    }
+}
