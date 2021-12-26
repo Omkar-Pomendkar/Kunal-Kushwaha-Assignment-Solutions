3e1cc29b343a442e6ee39ef66bcfb926e29dd83c
Omkar-Pomendkar <omkarpomendkar100@gmail.com>
2021-12-26 18:42:18 +0530

    helloworld

---
+++ 10-recursion/src/HelloWorlFunction.java
@@ -0,0 +1,35 @@
+public class HelloWorlFunction {
+    public static void main(String[] args) {
+        Greet();
+        //System.out.println();
+    }
+    static void Greet()
+    {
+        System.out.println("Hello Omkar");
+        Greet1();
+    }
+    static void Greet1()
+    {
+        System.out.println("Hello World");
+        Greet2();
+    }
+    static void Greet2()
+    {
+        System.out.println("Hello World");
+        Greet3();
+    }
+    static void Greet3()
+    {
+        System.out.println("Hello World");
+        Greet4();
+    }
+    static void Greet4()
+    {
+        System.out.println("Hello World");
+        Greet5();
+    }
+    static void Greet5()
+    {
+        System.out.println("Hello World");
+    }
+}
