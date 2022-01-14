89e7b6f3db1c92dd162eb2391edd20e9b9a5a44a
Omkar-Pomendkar <omkarpomendkar100@gmail.com>
2022-01-14 20:06:23 +0530

    Ceiling of a Number

--- 06-searching/src/CeilingofNumber.java
+++ 06-searching/src/CeilingofNumber.java
@@ -1,36 +1,44 @@
 public class CeilingofNumber {
     public static void main(String[] args) {
-        int[] arr = {2,3,5,9,14,16,18};
-        int target  = 10;
+        int [] arr = {2,3,5,6,14,16,18};
+        int target = 19;
         int ans = ceiling(arr,target);
         System.out.println(ans);
     }
-    static int ceiling(int[] arr,int target)
+    static int ceiling(int[] arr, int target)
     {
-        if(target > arr[arr.length-1])
+        //but what if target is greater then last index
+        //edge case
+        if (target > arr[arr.length-1]) {
+            return -1;
+
+        }
+        if(arr.length == 0)
         {
             return -1;
         }
         int start = 0;
         int end = arr.length-1;
-         while(start <= end)
-         {
-             int mid = start+(end-start)/2;
-             if(target < arr[mid])
-             {
-                 end = mid-1;
-             }
-             else if(target > arr[mid])
+
+        while (start<=end)
+        {
+            int mid = start+(end-start)/2;
+
+
+             if(target > arr[mid])
              {
                  start = mid+1;
+             }
+             else if(target < arr[mid])
+             {
+                 end = mid-1;
              }
              else
              {
                  return mid;
              }
-         }
-         return start;
+
+        }
+        return arr[start];
     }
-
-
 }
