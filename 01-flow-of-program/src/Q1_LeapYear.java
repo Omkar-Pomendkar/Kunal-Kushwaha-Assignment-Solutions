11b7dd1ac9e8c69b975aee49fdb58277e70cdc0e
Omkar-Pomendkar <omkarpomendkar100@gmail.com>
2022-01-14 15:36:32 +0530

    Change Added in Leap Year

--- 01-flow-of-program/src/Q1_LeapYear.java
+++ 01-flow-of-program/src/Q1_LeapYear.java
@@ -7,7 +7,7 @@ public class Q1_LeapYear {
         System.out.println("Enter a Year");
         int year = sc.nextInt();
         /* Logic of the Program
-           1) If year is Divsible by 4 it is Leap year
+           1) If year is Divisible by 4 it is Leap year
            2) But If it is Century Year then we have to see it is Divisible by 100 or Not
            3) If it is Completely Divisible 100 then it should be divsible by 400 then print leap year
 
