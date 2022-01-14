767d4241e987a842fb93f039178f7b1448d0c65f
Omkar-Pomendkar <omkarpomendkar100@gmail.com>
2022-01-14 20:16:26 +0530

    Bubble Sort

--- 07-sorting/src/BubbleSort.java
+++ 07-sorting/src/BubbleSort.java
@@ -2,32 +2,38 @@ import java.util.Arrays;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        System.out.println("Bubble Sort");
-        int[] arr = {1,2,3,4,5};
-        bubble(arr);
+        int [] arr ={1,2,3,4,5};
+        bubbleSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
-    static void bubble(int[] arr) {// run the steps n-1 times
-        boolean swapped ;
+    static void bubbleSort(int[] arr) {
+        boolean swapped;
+        //run the steps n times
         for (int i = 0; i < arr.length; i++) {
-            swapped = false;
-            // for ech step max iteam will come at the last respective index
-            for (int j = 1; j < arr.length - i; j++) {
-//                swap if the iteams is smaller then the previous item
-                if (arr[j] < arr[j - 1]) {
+            swapped = false; // if u did not swap break the loop
+            //for each steps, max item will come at last respective index
+            for (int j = 1; j < arr.length-i ; j++) {
+                //swap if the item is smaller then previous
+                if(arr[j] <arr[j-1])
+                {
                     int temp = arr[j];
                     arr[j] = arr[j-1];
                     arr[j-1] = temp;
                     swapped = true;
                 }
-
+                
             }
-            //if u did not swap for a particular value of i,it means the array is Sorted hence stop the program
-            if(!swapped) //!false = true
+            //if you did not swap for particular value of i,it means array is sorted hence stop the loop
+
+            if(!swapped)
             {
+
                 break;
             }
+
         }
     }
+
+
 }
