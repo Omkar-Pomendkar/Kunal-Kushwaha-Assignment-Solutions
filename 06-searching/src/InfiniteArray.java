b57af03e2465d9136b90e9b9c37cb8cd732934b3
Omkar-Pomendkar <omkarpomendkar100@gmail.com>
2022-01-14 20:08:41 +0530

    InfiniteArray

--- 06-searching/src/InfiniteArray.java
+++ 06-searching/src/InfiniteArray.java
@@ -1,45 +1,40 @@
 public class InfiniteArray {
     public static void main(String[] args) {
-        System.out.println("Find Positon of an element in infinite Sorted Array ");
-        int[] arr = {1,3,4,5,6,7,9,10};
-        int target = 6;
-        System.out.println(ans(arr,target));
-
+        int[] arr = { 55,2,4,6,7,32,45,65,68,99};
+        int target = 32;
+        int ans = ans(arr,target);
+        System.out.println(ans);
     }
-    static int ans(int[] arr,int target)
+    static int ans(int[] arr, int target)
     {
-        //first find rang
-        //first start with box of size 2
+        // Find the Range
+        // First start with box of size 2
         int start = 0;
         int end = 1;
-        //condition for target to lie in a range
-        while (target > arr[end])
-        {
-            int temp = end+1;
-            //double the box value
-            //end = previous end + size of box*2
-            end = end +(end -start+1) * 2;
-            start = temp;
 
+        //condition for target to lie in range is what
+        while(target > arr[end])
+        {
+            int temp = end+1;// this is new Start
+            // double the box value
+            // end = previous end +size of box * 2;
+            end = end +(end-start+1)*2;
+            start = temp ;
         }
-        return infinitearray(arr,target,start,end);
+        return binarysearch(arr,target,start,end);
     }
-    static int infinitearray(int[] arr, int target ,int start,int end)
-    {
+    static int binarysearch(int[] arr, int target ,int start ,int end) {
 
-        while(start<= end)
-        {
-            int mid = start+(end-start)/2;
-            if(target > arr[mid])
-            {
-                start = mid +1;
-            }
-            else if (target < arr[mid])
-            {
-                end = mid-1;
-            }
-            else
-            {
+        while(start <= end) {
+            //find Middle Element
+            int mid = start + (end - start) / 2;
+
+            if (target > arr[mid]) {
+                start = mid + 1;
+            } else if (target < arr[mid]) {
+                end = mid - 1;
+            } else {
+                //ans found
                 return mid;
             }
         }
