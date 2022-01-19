74b4c3922cb19311bee808dde6988785c1d394a7
Omkar-Pomendkar <omkarpomendkar100@gmail.com>
2022-01-19 16:04:10 +0530

    Class Example 5

---
+++ 14-OOP/src/Class/osp_05_Example5.java
@@ -0,0 +1,22 @@
+package Class;
+class TommyVecetti
+{
+    public void hitting()
+    {
+        System.out.println("Hitting....");
+    }
+
+    public void running()
+    {
+        System.out.println("running....");
+    }
+}
+public class osp_05_Example5 {
+    public static void main(String[] args) {
+        System.out.println(" Class Example 5");
+        TommyVecetti t = new TommyVecetti();
+        t.running();
+        t.hitting();
+
+    }
+}
